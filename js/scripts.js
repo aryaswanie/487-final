4353c4e8d8e66b36f2ae19adf2c01b61c1293387
aryaswanie <35304729+aryaswanie@users.noreply.github.com>
2018-12-11 17:00:08 -0500

    Finished the Brenda function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
   console.log('Ready to begin the test...');
 
+// button click effect
   $( ".mobile-btn" ).click(function() {
       $( ".mobile_nav" ).slideToggle( "slow" );
   $( ".mobile_nav" ).css("display", "flex");
+  });
 
-});
+// student objects
+
+  var student = {
+    name:'Brenda',
+    age:'20',
+    likesCoding:true,
+  }
+
+  if (student.likesCoding==true) {console.log(student.name + ', '+ student.age);}
+
 
 });
